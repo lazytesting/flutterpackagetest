849e49561ef86893e370ac00a894dee9f91ec8fe
Maarten Groeneweg <maartengroeneweg@funda.nl>
2023-09-15 09:33:13 +0200

    update renovate config

--- renovate-config.js
+++ renovate-config.js
@@ -1,25 +1,25 @@
 module.exports = {
-    branchPrefix: 'test-renovate/',
-    username: 'renovate-release',
-    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-    onboarding: false,
-    platform: 'github',
-    includeForks: false,
-    dryRun: 'full',
-    repositories: ['lazytesting/flutterpackagetest'],
-    packageRules: [
+    "branchPrefix": "test-renovate/",
+    "username": "renovate-release",
+    "gitAuthor": "Renovate Bot <bot@renovateapp.com>",
+    "onboarding": false,
+    "platform": "github",
+    "forkProcessing": "disabled",
+    "dryRun": "full",
+    "repositories": ["lazytesting/flutterpackagetest"],
+    "packageRules": [
       {
-        description: 'lockFileMaintenance',
-        matchUpdateTypes: [
-          'pin',
-          'digest',
-          'patch',
-          'minor',
-          'major',
-          'lockFileMaintenance',
+        "description": "lockFileMaintenance",
+        "matchUpdateTypes": [
+          "pin",
+          "digest",
+          "patch",
+          "minor",
+          "major",
+          "lockFileMaintenance"
         ],
-        dependencyDashboardApproval: false,
-        stabilityDays: 0,
-      },
-    ],
+        "dependencyDashboardApproval": false,
+        "minimumReleaseAge": null
+      }
+    ]
   };
